02d8804fd6625046d2b4cefe76f59c4f69ab6a2c
Maxime <maximebatista18@gmail.com>
2021-02-17 15:01:02 +0100

    Add project files.

---
+++ Controllers/BlueprintController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkit__NoBlock_IDE_WebApp.Controllers
+{
+    public class BlueprintController : Controller
+    {
+
+        private readonly ILogger<HomeController> logger;
+
+        public BlueprintController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            logger.LogInformation("Skurt :D");
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Linkit__NoBlock_IDE_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkit__NoBlock_IDE_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id, string test)
+        {
+            _logger.Log(LogLevel.Information, "This is a test message comming from Index: " + id);
+            _logger.LogInformation("test = " + test);
+            return View();
+        }
+
+        public IActionResult Privacy(int id)
+        {
+            _logger.Log(LogLevel.Information, "This is a test message comming from Privacy : " + id);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
